3b58f55afcc39e53119c9aef922a08123d6f2f4a
Deepika Rani <deepikasingh3729@gmail.com>
2025-08-23 20:25:51 +0530

    Added backend files with Mongo setup

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -5,6 +5,7 @@ import fs from 'fs'
 
 const addFood= async (req,res) => {
 
+
     let image_filename= `${req.file.filename}`;
       const food= new foodModel({
     name:req.body.name,
@@ -22,5 +23,34 @@ const addFood= async (req,res) => {
   }
 }
 
+// all food lists
 
-export {addFood}
+const listFood=async(req,res)=>{
+  try {
+    const foods=await foodModel.find({});
+    res.json({success:true,data:foods})
+  } catch(error){
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+}
+
+//remove food item
+const removeFood= async(req,res)=>{
+   try {
+    const food=await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`,()=>{})
+
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({success:true,message:"Food Removed"})
+  } catch(error){
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+
+}
+
+
+
+
+export {addFood,listFood,removeFood}
