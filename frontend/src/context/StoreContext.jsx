994489fba1e8a47a90b0e25267f263719d595dd9
Deepika Rani <deepikasingh3729@gmail.com>
2025-08-30 23:30:50 +0530

    fixed auth bug

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children}) => {
   const [cartItems, setCartItems] = useState({});
-  const url="http://localhost:4000/"
+  const url="http://localhost:4000"
   const [token,setToken]=useState("");
 
   const addToCart = (id) => {
