43468fd514e0034f06433b64a5ad8ee9835ef13c
Deepika Rani <deepikasingh3729@gmail.com>
2025-09-15 15:26:36 +0530

    create place order feature and stripe payment integration

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js";
 
 import 'dotenv/config'
+import orderRouter from "./routes/orderRoute.js"
 
 //app config
 const app = express()
@@ -29,6 +30,7 @@ app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order",orderRouter)
 
 
 
