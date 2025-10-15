48f1e0f68fbeeeda5471afdb70aab2eb644766b9
Deepika Rani <deepikasingh3729@gmail.com>
2025-10-15 21:12:47 +0530

    update

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,4 +1,5 @@
-import React, { useContext,  useState } from 'react';
+import React, { useContext,  useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; 
 import './PlaceOrder.css';
 import axios from 'axios';
 
