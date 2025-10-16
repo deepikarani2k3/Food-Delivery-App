19e6628e8d58b8ea1379d6006bf5f9f4e62c066d
Deepika Rani <deepikasingh3729@gmail.com>
2025-10-16 21:04:02 +0530

    refactor env files

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB= async()=>{
 
-  await mongoose.connect('mongodb+srv://deepikasingh:1318@cluster0.q4xej43.mongodb.net/food-del')
+  await mongoose.connect(process.env.MONGO_URI)
   .then(()=>console.log("DB connected"));
 }
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children}) => {
   const [cartItems, setCartItems] = useState({});
-  const url="http://localhost:4000"
+  const url= import.meta.env.VITE_BACKEND_URL;
   const [token,setToken]=useState("");
   const[food_list,setFoodList] =useState([])
 
